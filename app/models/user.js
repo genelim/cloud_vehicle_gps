9b9240f4657cb8eb2f6134515e5c8c090f4f962f
genelim <wernminlim@gmail.com>
2016-02-22 12:00:50 +0800

    vehicle model

---
+++ app/models/user.js
@@ -0,0 +1,46 @@
+//Database schema for user details
+module.exports = function (connection) {
+  	var mongoose = require('mongoose'),
+        bcrypt = require('bcrypt-nodejs');
+    
+    var contact = new mongoose.Schema({
+        telephone       : Number,
+        mobile_number   : Number,
+        fax_number      : Number
+	});
+    
+    var address = new mongoose.Schema({
+        address: String,
+        postcode: String,
+        city: String,
+        state: String,        
+        country: String,        
+    });
+    
+  	var user = new mongoose.Schema({
+        username         : String,
+        email            : String,
+        password         : String,
+		first_name       : String,
+		last_name        : String,
+        profile_image    : String,
+        contact          : contact,
+        address          : address,
+        company          : String,
+        note             : String,
+        role             : [Number],
+		updated_date     : Date, 
+		created_date     : { type : Date, default: Date.now },
+        status           : { type : Boolean, default: false }
+	});
+
+    user.methods.generateHash = function(password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    };
+
+    user.methods.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+  	return connection.model('User', user);
+}
---
+++ app/models/vehicle.js
@@ -0,0 +1,12 @@
+//Database schema for vehicle details
+module.exports = function (connection) {
+  	var mongoose = require('mongoose');
+    
+  	var vehicle = new mongoose.Schema({
+          vehicle_number    : String,
+          created_by        : { type: Schema.Types.ObjectId, ref: 'User' },
+          created_date      : { type : Date, default: Date.now }
+	});
+    
+  	return connection.model('Vehicle', vehicle);
+}

53a8650cdda2698cfaa58a7fb8c7211f128057cc
Gene Lim <Gene Lim>
2016-03-15 15:20:19 +0800

    Multi Vehicle Tracking Google Map Initialized

--- public/app/dashboard/vehicle_tracking.js
+++ public/app/dashboard/vehicle_tracking.js
@@ -5,10 +5,20 @@ angular
 DashboardVehicleTrackingController.$inject = [];
 
 function DashboardVehicleTrackingController(){
+    var vm = this;
+    vm.number_of_views = 4;
+    vm.get_number = get_number;
 
-    function map_initialize(){
-        setTimeout(function(){ google.maps.event.trigger(map, "resize") }, 100);
-        vm.current_tab = 'map';
+    angular.element(document).ready(function () {
+        for(var i = 0; i < vm.number_of_views; i++){
+            var map = new google.maps.Map(document.getElementById("map_home"+i));
+            map.setZoom(12);
+            map.setCenter({lat: -34.397, lng: 150.644});
+        }
+    })
+    
+    function get_number(number){
+        return new Array(number);   
     }
 
 }

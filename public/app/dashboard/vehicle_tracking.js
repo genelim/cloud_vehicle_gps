9b2f3beeb29ec2123a3bb1e2c4c758eb65d68c61
Dennis Lee <Dennis Lee>
2016-03-15 14:31:32 +0800

    Ui for multiple tracking

--- public/app/dashboard/vehicle_tracking.js
+++ public/app/dashboard/vehicle_tracking.js
@@ -6,4 +6,9 @@ DashboardVehicleTrackingController.$inject = [];
 
 function DashboardVehicleTrackingController(){
 
+    function map_initialize(){
+        setTimeout(function(){ google.maps.event.trigger(map, "resize") }, 100);
+        vm.current_tab = 'map';
+    }
+
 }

ed831842f0053fc16cf0caebba463690c3a027f0
Gene Lim <Gene Lim>
2016-04-06 17:18:46 +0800

    Clean code gprsFlow

--- public/app/report/gprs_flow.js
+++ public/app/report/gprs_flow.js
@@ -8,11 +8,7 @@ function GprsFlowController(){
     var vm = this;
     vm.test = test;
     vm.date = null;
-    function test(date){
+    function test(){
         console.log(vm.date)
     }
-     $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
 }
